9694244080ffc2ef61cd705dea088dcc7afb38a5
이민규 <hongb0707@gmail.com>
2022-04-15 16:47:02 +0900

    모바일 화면 우측 여백 수정

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,15 @@ import Footer from '../components/footer';
 import Header from '../components/header';
 import '../styles/globals.css'
 import 'semantic-ui-css/semantic.min.css'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Header />
+      <Head>
+        <title>TS&P</title>
+      </Head>
       <Component {...pageProps} />
       <Footer />
     </>
